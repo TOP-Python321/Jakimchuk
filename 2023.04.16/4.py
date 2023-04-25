bdb03a6544f81a18cb8e8d6ef8062d21459a378c
Bychyrek <nkitaplay228@mail.ru>
2023-04-25 19:32:51 +0400

    Исправления

--- 2023.04.16/4.py
+++ 2023.04.16/4.py
@@ -4,7 +4,7 @@ second_cell = input('Введите координату второй клетк
 # КОММЕНТАРИЙ: слишком много лишних скобок, здесь они уже объективно затрудняют чтение — помедитируйте ещё раз над таблицей о приоритетах операторов:
 #  https://docs.python.org/3/reference/expressions.html#operator-precedence
 # КОММЕНТАРИЙ: вам очень повезло, что код символа 'a' является нечётным числом =)
-if (((ord(first_cell[0]) + int(first_cell[1])) % 2) == (ord(second_cell[0]) + int(second_cell[1])) % 2):
+if (ord(first_cell[0]) + int(first_cell[1])) % 2 == (ord(second_cell[0]) + int(second_cell[1])) % 2:
     print('Да')
 else:
     print('Нет')
